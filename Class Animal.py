f34d2e01913beb612186a55003df8ce3700aa180
Mayra30345 <palmayra966@gmail.com>
2025-10-04 17:06:10 +0530

    Add files via upload

---
+++ Class Animal.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    def move(self):
+        pass
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+class Snake(Animal):
+    def move(self):
+        print("I can crawl.")
+class Dog(Animal):
+    def move(self):
+        print("I can Bark.")
+class Lion(Animal):
+    def move(self):
+        print("I can roar")
+R=Human()
+K=Snake()
+M=Dog()
+O=Lion()
+for n in (R,K,M,O):
+    n.move()
